164c274d455bfdd712ab91c2424298d2507409b0
lMasterSparkl <592712515@qq.com>
2020-12-28 14:51:21 +0800

    添加 层序遍历二叉树及每层输出一个数组 问题

--- src/main/java/com/ygt/tree/solve/VisitTree.java
+++ src/main/java/com/ygt/tree/solve/VisitTree.java
@@ -2,8 +2,13 @@ package com.ygt.tree.solve;
 
 import com.ygt.tree.entity.NormalTreeNode;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * 各种遍历二叉树方法
+ *
  * @author 伊格田
  * @date 2020/12/25
  */
@@ -21,7 +26,66 @@ public class VisitTree {
 
     }
 
+    /**
+     * 层序遍历 二叉树 通过队列实现
+     *
+     * @param root
+     * @return
+     */
+    public ArrayList<Integer> printFromTopToBottom(NormalTreeNode root) {
+        Queue<NormalTreeNode> queue = new LinkedList<>();
+        ArrayList<Integer> ret = new ArrayList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            int cnt = queue.size();
+            while (cnt-- > 0) {
+                NormalTreeNode t = queue.poll();
+                if (t == null) {
+                    continue;
+                }
+                ret.add(t.getVal());
+                queue.add(t.getLeft());
+                queue.add(t.getRight());
+            }
+        }
+        return ret;
+    }
+
+    /**
+     * 按行打印 层序遍历
+     * 从上到下按层打印二叉树，同一层结点从左至右输出。每一层输出一行。
+     * 示例1
+     * 输入
+     * {8,6,10,5,7,9,11}
+     * 返回值
+     * [[8],[6,10],[5,7,9,11]]
+     * @param pRoot
+     * @return
+     */
+    ArrayList<ArrayList<Integer>> linePrint(NormalTreeNode pRoot) {
+        ArrayList<ArrayList<Integer>> ret = new ArrayList<>();
+        Queue<NormalTreeNode> queue = new LinkedList<>();
+        queue.add(pRoot);
+        while (!queue.isEmpty()) {
+            ArrayList<Integer> list = new ArrayList<>();
+            int cnt = queue.size();
+            while (cnt-- > 0) {
+                NormalTreeNode node = queue.poll();
+                if (node == null) {
+                    continue;
+                }
+                list.add(node.getVal());
+                queue.add(node.getLeft());
+                queue.add(node.getRight());
+            }
+            if (list.size() != 0) {
+                ret.add(list);
+            }
+        }
+        return ret;
+    }
+
     private void visit(NormalTreeNode root) {
-        System.out.println(" 二叉树节点值为："+root.getVal());
+        System.out.println(" 二叉树节点值为：" + root.getVal());
     }
 }
--- src/test/java/com/ygt/tree/solve/VisitTreeTest.java
+++ src/test/java/com/ygt/tree/solve/VisitTreeTest.java
@@ -3,17 +3,51 @@ package com.ygt.tree.solve;
 import com.ygt.tree.entity.NormalTreeNode;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+
 /**
  * @author 伊格田
  * @date 2020/12/25
  */
 class VisitTreeTest {
-    private VisitTree visitTree= new VisitTree();
+    private VisitTree visitTree = new VisitTree();
+
+    /**
+     * 中序遍历
+     */
     @Test
     void inorderTraversal() {
-        NormalTreeNode left = new NormalTreeNode(2,null,null);
-        NormalTreeNode right = new NormalTreeNode(4,null,null);
-        NormalTreeNode normalTreeNode = new NormalTreeNode(3,left,right);
+        NormalTreeNode left = new NormalTreeNode(2, null, null);
+        NormalTreeNode right = new NormalTreeNode(4, null, null);
+        NormalTreeNode normalTreeNode = new NormalTreeNode(3, left, right);
         visitTree.inorderTraversal(normalTreeNode);
+    }
+
+
+    /**
+     * 层序遍历测试
+     */
+    @Test
+    void testPrintFromTopToBottom() {
+        //        int[] temp = {1, 2, 3, 4, 5, 6, 7, 8};
+        int[] temp = {1, 2, 2, 3, 4, 4, 3};
+        NormalTreeNode normalTreeNode = NormalTreeNode.putAll(temp, 0);
+        VisitTree visitTree = new VisitTree();
+        ArrayList<Integer> integers = visitTree.printFromTopToBottom(normalTreeNode);
+        System.out.println(integers);
+        //[1, 2, 2, 3, 4, 4, 3]
+    }
+
+    /**
+     * 层序遍历每层一个数组
+     */
+    @Test
+    void linePrint() {
+        //        int[] temp = {1, 2, 3, 4, 5, 6, 7, 8};
+        int[] temp = {1, 2, 2, 3, 4, 4, 3};
+        NormalTreeNode normalTreeNode = NormalTreeNode.putAll(temp, 0);
+        VisitTree visitTree = new VisitTree();
+        ArrayList<ArrayList<Integer>> arrayLists = visitTree.linePrint(normalTreeNode);
+        System.out.println(arrayLists);
     }
 }
